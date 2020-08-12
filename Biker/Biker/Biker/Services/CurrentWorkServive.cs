5c55a4ff6339128ac1053eddd5f62eba1dcc3303
parada <parada@perfenterprise.com>
2020-08-12 10:47:02 +0700

    Add Current work service.

---
+++ Biker/Biker/Biker/Services/CurrentWorkServive.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biker.Services
+{
+    public class CurrentWorkServive
+    {
+        static SecureStorageService secureStorage = new SecureStorageService();
+        static string WORKID_KEY = "CurrentWork";
+
+        public static async Task<string> GetCurrentWork()
+        {
+            var hasCurrentWork = await secureStorage.Contains(WORKID_KEY);
+            if (hasCurrentWork)
+            {
+                var result = await secureStorage.Retrieve(WORKID_KEY);
+                return result;
+            }
+            else
+            {
+                return "";
+            }
+        }
+
+        public static async Task AddCurrentWork(string workId)
+        {
+            await secureStorage.Store(WORKID_KEY, workId);
+        }
+
+        public static void ClearCurrntWork()
+        {
+            secureStorage.Delete(WORKID_KEY);
+        }
+    }
+}
---
+++ Biker/Biker/Biker/Services/SecureStorageService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using xames = Xamarin.Essentials;
+
+namespace Biker.Services
+{
+    public class SecureStorageService
+    {
+        public async Task<bool> Contains(string key)
+        {
+            var data = await xames.SecureStorage.GetAsync(key);
+            return data != null;
+        }
+
+        public async void Delete(string key)
+        {
+            if (await Contains(key))
+            {
+                xames.SecureStorage.Remove(key);
+            }
+        }
+
+        public async Task<string> Retrieve(string key)
+        {
+            return await xames.SecureStorage.GetAsync(key);
+        }
+
+        public async Task Store(string key, string data)
+        {
+            await xames.SecureStorage.SetAsync(key, data);
+        }
+    }
+}
